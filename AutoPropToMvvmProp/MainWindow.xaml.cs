1bc4c21cb37343239599fad2a1f1387fb594a428
Al <al.pankratz@hotmail.de>
2017-11-21 14:11:59 +0100

    A bit of code restructoring. Added some descriptive comments

--- AutoPropToMvvmProp/MainWindow.xaml.cs
+++ AutoPropToMvvmProp/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace AutoPropToMvvmProp
     public partial class MainWindow : Window
     {
         private StringBuilder convertedProperties;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,23 +31,15 @@ namespace AutoPropToMvvmProp
             FillMvvmStyles();
         }
 
-        private void FillMvvmStyles()
-        {
-            var mvvmStyles = new List<string>()
-            {
-                "DevExpress",
-                "MVVM Light"
-            };
-            this.cbxStyles.ItemsSource = null;
-            this.cbxStyles.ItemsSource = mvvmStyles;
-            this.cbxStyles.SelectedIndex = 0;
-        }
-
+        #region Button Click Event Handler
         private void btnDo_Click(object sender, RoutedEventArgs e)
         {
             convertedProperties = new StringBuilder();
 
+            // Auto Properties should be separated by line
             string[] propTextPerLine = Regex.Split(tbxProp.Text, "\r\n");
+
+            // check what style was selected and convert every line of the auto properties
             string stylesSelection = cbxStyles.SelectedItem.ToString();
             foreach (string line in propTextPerLine)
             {
@@ -54,76 +47,6 @@ namespace AutoPropToMvvmProp
             }
             tbxMvvm.Text = convertedProperties.ToString();
             convertedProperties = new StringBuilder();
-        }
-
-        private void ConvertToMvvmProperty(string line, string style)
-        {
-            switch (style.ToLower())
-            {
-                case "devexpress":
-                    ConvertToDXProperty(line);
-                    break;
-
-                case "mvvm light":
-                    ConvertToLightProperty(line);
-                    break;
-
-                default:
-                    return;
-            }
-
-        }
-
-        private void ConvertToDXProperty(string line)
-        {
-            if (String.IsNullOrEmpty(line))
-                return;
-
-            try
-            {
-                line = line.TrimStart(' ');
-                string[] splittedLine = line.Split(' ');
-                string propertyName = splittedLine.ElementAt(2);
-                string propertyType = splittedLine.ElementAt(1);
-                string backingFieldName = $"_{propertyName.ToLower()}";
-                string backingField = $"private {propertyType} {backingFieldName};";
-
-                string getter = $"get {{ return {backingFieldName}; }}";
-                string setter = $"set {{ SetProperty(ref {backingFieldName}, value, () => {propertyName}); }}";
-
-                string dxProperty = $"{backingField}\npublic {propertyType} {propertyName}\n{{\n{getter}\n{setter}\n}}";
-                convertedProperties.AppendLine(dxProperty);
-            }
-            catch (Exception ex)
-            {
-                return;
-            }
-        }
-
-        private void ConvertToLightProperty(string line)
-        {
-            if (String.IsNullOrEmpty(line))
-                return;
-
-            try
-            {
-                line = line.TrimStart(' ');
-                string[] splittedLine = line.Split(' ');
-                string propertyName = splittedLine.ElementAt(2);
-                string propertyType = splittedLine.ElementAt(1);
-                string backingFieldName = $"_{propertyName.ToLower()}";
-                string backingField = $"private {propertyType} {backingFieldName};";
-
-                string getter = $"get {{ return {backingFieldName}; }}";
-                string setter = $"set {{ {backingFieldName} = value;\nRaisePropertyChanged(() => {propertyName}); }}";
-
-                string dxProperty = $"{backingField}\npublic {propertyType} {propertyName}\n{{\n{getter}\n{setter}\n}}";
-                convertedProperties.AppendLine(dxProperty);
-            }
-            catch (Exception ex)
-            {
-                return;
-            }
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
@@ -155,7 +78,7 @@ namespace AutoPropToMvvmProp
 
             if (File.Exists(file))
             {
-                if (MessageBox.Show("File already exists.\nOverwrite?", "File already exists", MessageBoxButton.YesNo,MessageBoxImage.Warning) == MessageBoxResult.No)
+                if (MessageBox.Show("File already exists.\nOverwrite?", "File already exists", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
                     return;
 
                 File.Delete(file);
@@ -166,10 +89,112 @@ namespace AutoPropToMvvmProp
                 stream.WriteLine(this.tbxMvvm.Text);
             }
         }
+        #endregion Button Click Event Handler
 
+        #region Private Methods
         private bool ValidateMvvmContent()
         {
             return String.IsNullOrEmpty(tbxMvvm.Text);
         }
+
+        // Fill the ComboBox 
+        private void FillMvvmStyles()
+        {
+            var mvvmStyles = new List<string>()
+            {
+                "DevExpress",
+                "MVVM Light"
+            };
+            this.cbxStyles.ItemsSource = null;
+            this.cbxStyles.ItemsSource = mvvmStyles;
+            this.cbxStyles.SelectedIndex = 0;
+        }
+        #endregion Private Methods
+
+        #region Converter Methods
+        /// <summary>
+        /// Method to validate the selected style and pass it through for convertion
+        /// </summary>
+        /// <param name="line">A string containing the Auto Propertie which should be converted</param>
+        /// <param name="style">A string, containing the selected style from the ComboBox</param>
+        private void ConvertToMvvmProperty(string line, string style)
+        {
+            // check the selected style. For security reasons, only validate on lowercase strings
+            switch (style.ToLower())
+            {
+                case "devexpress":
+                    ConvertToDXProperty(line);
+                    break;
+
+                case "mvvm light":
+                    ConvertToLightProperty(line);
+                    break;
+
+                default:
+                    return;
+            }
+
+        }
+
+        /// <summary>
+        /// Converter for DevExpress.Mvvm Properties
+        /// </summary>
+        /// <param name="line">A string containing the Auto Propertie which should be converted</param>
+        private void ConvertToDXProperty(string line)
+        {
+            if (String.IsNullOrEmpty(line))
+                return;
+
+            try
+            {
+                line = line.TrimStart(' ');
+                string[] splittedLine = line.Split(' ');
+                string propertyName = splittedLine.ElementAt(2);
+                string propertyType = splittedLine.ElementAt(1);
+                string backingFieldName = $"_{propertyName.ToLower()}";
+                string backingField = $"private {propertyType} {backingFieldName};";
+
+                string getter = $"get {{ return {backingFieldName}; }}";
+                string setter = $"set {{ SetProperty(ref {backingFieldName}, value, () => {propertyName}); }}";
+
+                string dxProperty = $"{backingField}\npublic {propertyType} {propertyName}\n{{\n{getter}\n{setter}\n}}";
+                convertedProperties.AppendLine(dxProperty);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Converter for MVVM Light Properties
+        /// </summary>
+        /// <param name="line">A string containing the Auto Propertie which should be converted</param>
+        private void ConvertToLightProperty(string line)
+        {
+            if (String.IsNullOrEmpty(line))
+                return;
+
+            try
+            {
+                line = line.TrimStart(' ');
+                string[] splittedLine = line.Split(' ');
+                string propertyName = splittedLine.ElementAt(2);
+                string propertyType = splittedLine.ElementAt(1);
+                string backingFieldName = $"_{propertyName.ToLower()}";
+                string backingField = $"private {propertyType} {backingFieldName};";
+
+                string getter = $"get {{ return {backingFieldName}; }}";
+                string setter = $"set {{ {backingFieldName} = value;\nRaisePropertyChanged(() => {propertyName}); }}";
+
+                string dxProperty = $"{backingField}\npublic {propertyType} {propertyName}\n{{\n{getter}\n{setter}\n}}";
+                convertedProperties.AppendLine(dxProperty);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+        #endregion Converter Methods
     }
 }
